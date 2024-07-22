4e9f43f65b27eb29c89e118b941cc4d751491201
jesus <Jesusgarba@gmail.com>
2024-07-22 07:58:59 +0200

    Config Drawer

--- app/src/main/java/com/example/myapplication/presentation/initScreen.kt
+++ app/src/main/java/com/example/myapplication/presentation/initScreen.kt
@@ -7,66 +7,86 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
 @Preview(showSystemUi = true)
 @Composable
 fun InitScreen() {
-    Scaffold(
-
-        topBar = { TopBarView() },
-
-
-        content = { pading ->
-            Column(
-                modifier = Modifier
-                    .padding(pading)
-            ) {
-                Text("Content", color = Color.White)
-            }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet { Text(modifier = Modifier.padding(8.dp), text = "PRUEBA", fontSize = 16.sp, fontWeight = FontWeight.Bold) }
         },
-    )
+    ) {
+        Scaffold(
+
+            topBar = { TopBarView(drawerState, scope) },
+
+
+            content = { pading ->
+                Column(
+                    modifier = Modifier
+                        .padding(pading)
+                ) {
+                    Text("Content", color = Color.White)
+                }
+            },
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBarView() {
+fun TopBarView(drawerState: DrawerState, scope: CoroutineScope) {
     Box(modifier = Modifier.fillMaxSize()) {
         TopAppBar(
             modifier = Modifier
                 .background(Color.White)
                 .shadow(elevation = 5.dp),
             navigationIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = {
+                    scope.launch {
+                        drawerState.apply {
+                            if (isClosed) open() else close()
+                        }
+                    }
+                }) {
                     Icon(imageVector = Icons.Rounded.Menu, contentDescription = "menú")
                 }
             },
