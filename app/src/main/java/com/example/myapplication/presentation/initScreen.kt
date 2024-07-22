8b049df381ecccc01f942521a9cec72fae576b9a
jesus <Jesusgarba@gmail.com>
2024-07-22 17:45:41 +0200

    dropdown menu ok

--- app/src/main/java/com/example/myapplication/presentation/initScreen.kt
+++ app/src/main/java/com/example/myapplication/presentation/initScreen.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication.presentation
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,10 +14,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,7 +32,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -36,6 +46,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
@@ -51,7 +62,14 @@ fun InitScreen() {
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet { Text(modifier = Modifier.padding(8.dp), text = "PRUEBA", fontSize = 16.sp, fontWeight = FontWeight.Bold) }
+            ModalDrawerSheet {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "PRUEBA",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         },
     ) {
         Scaffold(
@@ -87,7 +105,7 @@ fun TopBarView(drawerState: DrawerState, scope: CoroutineScope) {
                         }
                     }
                 }) {
-                    Icon(imageVector = Icons.Rounded.Menu, contentDescription = "menú")
+                    Icon(imageVector = Icons.Rounded.Menu, contentDescription = "menu")
                 }
             },
             title = { ImageAndTextAppBar() },
@@ -97,9 +115,13 @@ fun TopBarView(drawerState: DrawerState, scope: CoroutineScope) {
 
 @Composable
 fun ImageAndTextAppBar() {
-    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
         Spacer(modifier = Modifier.weight(0.5f))
-        Text("Dragon Ball API", textAlign = TextAlign.Center)
+        ConfigMenu()
         Spacer(modifier = Modifier.weight(0.5f))
         Box(
             modifier = Modifier
@@ -108,11 +130,79 @@ fun ImageAndTextAppBar() {
         ) {
             Image(
                 painterResource(id = R.drawable.icon_bola_drac),
-                contentDescription = "icon bola de drac",
+                contentDescription = "dragon ball icon",
                 contentScale = ContentScale.Fit
             )
         }
 
     }
 }
+
+@Composable
+fun ConfigMenu() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable {}) {
+        //Text("characters", fontSize = 16.sp)
+        ItemDropDrawMenu()
+        /*     Icon(
+                 modifier = Modifier.height(16.dp),
+                 imageVector = Icons.Rounded.KeyboardArrowDown,
+                 contentDescription = "icon"
+             )*/
+    }
+}
+
+@Composable
+fun ItemDropDrawMenu() {
+    var expanded by remember { mutableStateOf(false) }
+    val items = listOf("characters", "transformations", "planets")
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopStart)
+            .background(Color.White)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                expanded = true
+            }) {
+            Text(
+                text = items[selectedIndex],
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
+            Icon(
+                modifier = Modifier.height(16.dp),
+                imageVector = Icons.Rounded.KeyboardArrowDown,
+                contentDescription = "icon"
+            )
+        }
+
+        DropdownMenu(
+            modifier = Modifier.background(Color.White),
+            expanded = expanded,
+            offset = DpOffset(x = (-15).dp, y = (+10).dp),
+            onDismissRequest = { expanded = false },
+
+            ) {
+            items.forEachIndexed { index, s ->
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .background(Color.White),
+                    text = { Text(text = s, fontSize = 16.sp, fontWeight = FontWeight.Normal) },
+                    onClick = {
+                        selectedIndex = index
+                        expanded = false
+                    })
+            }
+        }
+    }
+}
+
+
+
 
