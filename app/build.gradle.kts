0f03a5649f541d2d7bd98cb949ae51738c7cdd62
schugarba <jesusgarba@gmail.com>
2024-12-05 07:40:29 +0100

    Update AGP in libs_versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
     kotlin("kapt")
     alias(libs.plugins.hilt)
 
