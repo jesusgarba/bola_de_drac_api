550e15c8a7443c10a03f42829bbe8941cdb799dd
jesus <Jesusgarba@gmail.com>
2025-01-25 17:55:24 +0100

    Navigation in process

---
+++ app/src/main/java/com/example/myapplication/core/navigation/NavigationWrapper.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.presentation.InitScreen
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+   // NavHost(navController = navController, startDestination = ){
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/core/navigation/Screens.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.core.navigation
+
+import com.google.gson.annotations.SerializedName
+
+@SerializedName
+object InitialScreen
